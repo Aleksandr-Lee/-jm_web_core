cc44cccde6144c4a07e435c84ce75228f530b664
Aleksandr Lee <lexxlee@rambler.ru>
2020-11-05 23:03:02 +0300

    редактирование кода

--- src/js/feedback-menu.js
+++ src/js/feedback-menu.js
@@ -26,23 +26,3 @@ window.addEventListener("click", function (event) {
     body.style.overflow = "visible";
   }
 });
-
-/*const modalFeedback = document.getElementById('modalFeedback');
-const openFeedback = document.getElementById('btnOpenFeedback');
-const closeFeedback = document.getElementById('btnCloseFeedback');
-const body = document.querySelector('body');
-
-openFeedback.onclick = function() {
-	modalFeedback.style.display = 'block';
-	body.style.overflow = 'hidden';
-};
-closeFeedback.onclick = function() {
-	modalFeedback.style.display = 'none';
-	body.style.overflow = 'visible';
-};
-window.onclick = function(event) {
-	if(event.target == modalFeedback) {
-		modalFeedback.style.display = 'none';
-		body.style.overflow = 'visible';
-	}
-};*/
